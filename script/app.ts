c13dbe808c89c58f7e654b93b532b1385197bdd0
Christian Ingles <christianarguetaaa@gmail.com>
2023-01-17 18:39:25 -0600

    Add references to HTML elements

---
+++ script/app.ts
@@ -0,0 +1,5 @@
+const colorHexa = document.querySelector('#colorHexa')
+const colorRgb = document.querySelector('#colorRgb')
+const colorSimple = document.querySelector('#colorSimple')
+const colorText = document.querySelector('#colorText')
+const btnColorRandom = document.querySelector('#btnColorRandom')
---
+++ script/build/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const colorHexa = document.querySelector('#colorHexa');
+const colorRgb = document.querySelector('#colorRgb');
+const colorSimple = document.querySelector('#colorSimple');
+const colorText = document.querySelector('#colorText');
+const btnColorRandom = document.querySelector('#btnColorRandom');
