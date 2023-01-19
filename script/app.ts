a02803e69c388c4edd8d83539b77f5579e6ab6e7
Christian Ingles <christianarguetaaa@gmail.com>
2023-01-18 20:31:42 -0600

    Add functions for the ditintos type of numbers

--- script/app.ts
+++ script/app.ts
@@ -3,8 +3,27 @@ const colorRgb = document.querySelector('#colorRgb')
 const colorSimple = document.querySelector('#colorSimple')
 const colorText = document.querySelector('#colorText')
 const btnColorRandom = document.querySelector('#btnColorRandom')
-
+const colors = ['Red', 'Blue', 'Green', 'Yellow', 'Pink']
 let colorType: string = "hexadecimal"
+
+const randomNumberGenerator = (typeNumber: string) => {
+    let numberRandom: number = 0;
+
+    if (typeNumber == "rgb") {
+        numberRandom = Math.floor(Math.random() * (255 + 1))
+        return numberRandom;
+    }
+
+    if (typeNumber == "hexadecimal") {
+        numberRandom = Math.floor(Math.random() * (16 + 1))
+        return numberRandom.toString(16);
+    }
+
+    if (typeNumber == "simple text") {
+        numberRandom = Math.floor(Math.random() * (5 + 1))
+        return numberRandom;
+    }
+}
 
 colorHexa?.addEventListener('click', () => {
     colorType = "hexadecimal"
--- script/build/app.js
+++ script/build/app.js
@@ -4,16 +4,29 @@ const colorRgb = document.querySelector('#colorRgb');
 const colorSimple = document.querySelector('#colorSimple');
 const colorText = document.querySelector('#colorText');
 const btnColorRandom = document.querySelector('#btnColorRandom');
+const colors = ['Red', 'Blue', 'Green', 'Yellow', 'Pink'];
 let colorType = "hexadecimal";
+const randomNumberGenerator = (typeNumber) => {
+    let numberRandom = 0;
+    if (typeNumber == "rgb") {
+        numberRandom = Math.floor(Math.random() * (255 + 1));
+        return numberRandom;
+    }
+    if (typeNumber == "hexadecimal") {
+        numberRandom = Math.floor(Math.random() * (16 + 1));
+        return numberRandom.toString(16);
+    }
+    if (typeNumber == "simple text") {
+        numberRandom = Math.floor(Math.random() * (5 + 1));
+        return numberRandom;
+    }
+};
 colorHexa === null || colorHexa === void 0 ? void 0 : colorHexa.addEventListener('click', () => {
     colorType = "hexadecimal";
-    console.log(colorType);
 });
 colorRgb === null || colorRgb === void 0 ? void 0 : colorRgb.addEventListener('click', () => {
     colorType = "rgb";
-    console.log(colorType);
 });
 colorSimple === null || colorSimple === void 0 ? void 0 : colorSimple.addEventListener('click', () => {
     colorType = "simple text";
-    console.log(colorType);
 });
